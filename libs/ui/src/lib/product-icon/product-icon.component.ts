ffe49d85d78aecd476b5e5a8c99a2a045230143c
gen8media <68166624+gen8media@users.noreply.github.com>
2021-02-02 17:58:28 -0500

    Add files via upload

---
+++ libs/ui/src/lib/product-icon/product-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-workspace-product-icon',
+  templateUrl: './product-icon.component.html',
+  styleUrls: ['./product-icon.component.scss']
+})
+export class ProductIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
